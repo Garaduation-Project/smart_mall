4f6b508555bfb0c21b576108ef040ada44b4118c
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-06-25 22:48:53 +0300

    succesfullybooked.dart

--- lib/succesfullybooked.dart
+++ lib/succesfullybooked.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:parking/bookings.dart';
+// ignore: unused_import
+import 'package:parking/homePage.dart';
 
 void main() {
   runApp(SuccessPage());
@@ -18,16 +23,32 @@ class Successful extends StatelessWidget {
 }
 
 class SuccessPage extends StatelessWidget {
+  const SuccessPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QR Code'),
-        backgroundColor: Color.fromRGBO(172, 162, 176, 0.1),
+        actions: [
+          IconButton(
+            color: Color.fromRGBO(88, 80, 141, 1),
+            icon: const Icon(Icons.home),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              );
+            },
+          ),
+        ],
+        title: Text(''),
+        backgroundColor: Color.fromRGBO(172, 162, 176, 0.3),
       ),
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.only(top: 10.0, bottom: 500),
+        padding: const EdgeInsets.only(top: 0.0, bottom: 10),
         child: Center(
           child: Padding(
             padding: const EdgeInsets.only(top: 10.0),
@@ -47,6 +68,40 @@ class SuccessPage extends StatelessWidget {
                 Text(
                   'Successfully booked!',
                   style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 40, 10, 40),
+                  child: Center(
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BookingPage(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        ' Go To Your Bookings',
+                        style: TextStyle(
+                          color: Color.fromRGBO(238, 238, 238, 1),
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        side: BorderSide(
+                            color: Color.fromRGBO(88, 80, 141, 1), width: 1.0),
+                      ),
+                      color: Color.fromRGBO(88, 80, 141, 1),
+                      textColor: Colors.black,
+                      padding: EdgeInsets.fromLTRB(10, 16, 10, 16),
+
+                      //
+                      minWidth: 300,
+                    ),
+                  ),
                 ),
               ],
             ),
