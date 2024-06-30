959618ec12f5f6bdd36d30bbab67b1b87928f69a
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-07-01 00:34:18 +0300

    Update pass_reset.dart

--- lib/pass_reset/pass_reset.dart
+++ lib/pass_reset/pass_reset.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:parking/logIN.dart';
+import 'package:parking/models/pass_reset_api_model.dart';
 
-class ResetPasswordPage extends StatelessWidget {
+class ResetPasswordPage extends StatefulWidget {
+  @override
+  _ResetPasswordPageState createState() => _ResetPasswordPageState();
+}
+
+class _ResetPasswordPageState extends State<ResetPasswordPage> {
+  final TextEditingController _codeController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+  String? _message;
+
+  void _resetPassword() async {
+    setState(() {
+      _isLoading = true;
+      _message = null;
+    });
+
+    final resetToken = _codeController.text;
+    final newPassword = _passwordController.text;
+    final response =
+        await PasswordResetAPI.resetPassword(resetToken, newPassword);
+
+    setState(() {
+      _isLoading = false;
+      _message = response['message'];
+    });
+
+    if (response['success']) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => NameScreen()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,6 +75,7 @@ class ResetPasswordPage extends StatelessWidget {
             ),
             SizedBox(height: 40),
             TextField(
+              controller: _codeController,
               decoration: InputDecoration(
                 labelText: '  Confirmation code',
                 border: OutlineInputBorder(
@@ -49,6 +85,7 @@ class ResetPasswordPage extends StatelessWidget {
             ),
             SizedBox(height: 40),
             TextField(
+              controller: _passwordController,
               decoration: InputDecoration(
                 labelText: '  New password',
                 border: OutlineInputBorder(
@@ -58,40 +95,48 @@ class ResetPasswordPage extends StatelessWidget {
               obscureText: true,
             ),
             SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NameScreen()),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(30, 40, 30, 50),
-                child: Container(
-                  width: 200,
-                  height: 60,
-                  decoration: ShapeDecoration(
-                    // color:
-                    color: Color.fromRGBO(88, 80, 141, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Reset',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontFamily: 'Cantoraone',
-                        height: 0,
+            _isLoading
+                ? CircularProgressIndicator()
+                : GestureDetector(
+                    onTap: _resetPassword,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 40, 30, 50),
+                      child: Container(
+                        width: 200,
+                        height: 60,
+                        decoration: ShapeDecoration(
+                          color: Color.fromRGBO(88, 80, 141, 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Reset',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                              fontFamily: 'Cantoraone',
+                              height: 0,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
+            if (_message != null)
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  _message!,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
           ],
         ),
       ),
