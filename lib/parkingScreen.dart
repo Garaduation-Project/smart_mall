40f1ef6f2c7a79632bfee95c8b8c4a8a3ac726d3
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-07-03 02:09:40 +0300

    Update parkingScreen.dart

--- lib/parkingScreen.dart
+++ lib/parkingScreen.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:parking/homePage.dart';
+import 'package:parking/homePage.dart'; 
+import 'package:parking/models/reserve_api_model.dart';
 import 'package:parking/succesfullybooked.dart';
 
 class ReservationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Reservation',
-            style: TextStyle(
-              color: Color.fromRGBO(88, 80, 141, 1),
-              fontFamily: 'Pacifico',
-              fontSize: 26,
-            ),
+      appBar: AppBar(
+        title: Text(
+          'Reservation',
+          style: TextStyle(
+            color: Color.fromRGBO(88, 80, 141, 1),
+            fontFamily: 'Pacifico',
+            fontSize: 26,
           ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomePage(),
+              ),
+            );
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.home),
             onPressed: () {
               Navigator.push(
                 context,
@@ -26,52 +39,59 @@ class ReservationPage extends StatelessWidget {
               );
             },
           ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomePage(),
+        ],
+        backgroundColor: Color.fromRGBO(172, 162, 176, 0.3),
+      ),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: BoxDecoration(color: Colors.white),
+              height: 350,
+              child: Stack(
+                children: <Widget>[
+                  Image.asset(
+                    'images/reserve_page.png',
+                    width: MediaQuery.of(context).size.width,
+                    height: 400,
+                    fit: BoxFit.fitWidth,
                   ),
-                );
-              },
-            ),
-          ],
-          backgroundColor: Color.fromRGBO(172, 162, 176, 0.3),
-        ),
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.only(bottom: 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                decoration: new BoxDecoration(color: Colors.white),
-                height: 350,
-                child: Stack(
-                  children: <Widget>[
-                    Image.asset(
-                      'images/reserve_page.png',
-                      width: MediaQuery.of(context).size.width,
-                      height: 400,
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ],
-                ),
+                ],
               ),
-              SizedBox(
-                height: 210,
-                child: Padding(
-                  padding:
-                      const EdgeInsets.only(top: 0.0, bottom: 10, left: 40),
-                  child: Center(
-                    child: RichText(
-                      maxLines: 2,
-                      text: const TextSpan(
-                          text: "Kindly ",
+            ),
+            SizedBox(
+              height: 210,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 0.0, bottom: 10, left: 40),
+                child: Center(
+                  child: RichText(
+                    maxLines: 2,
+                    text: const TextSpan(
+                      text: "Kindly ",
+                      style: TextStyle(
+                        wordSpacing: 4,
+                        color: Colors.black,
+                        fontSize: 28,
+                        fontFamily: 'CantoraOne',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Press Reserve To",
+                          style: TextStyle(
+                            wordSpacing: 4,
+                            color: Color.fromRGBO(88, 80, 141, 1),
+                            fontSize: 28,
+                            fontFamily: 'CantoraOne',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: " Get Your Code ",
                           style: TextStyle(
                             wordSpacing: 4,
                             color: Colors.black,
@@ -79,79 +99,71 @@ class ReservationPage extends StatelessWidget {
                             fontFamily: 'CantoraOne',
                             fontWeight: FontWeight.bold,
                           ),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: "Press Reserve To",
-                              style: TextStyle(
-                                wordSpacing: 4,
-                                color: Color.fromRGBO(88, 80, 141, 1),
-                                fontSize: 28,
-                                fontFamily: 'CantoraOne',
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            TextSpan(
-                              text: " Get Your Code ",
-                              style: TextStyle(
-                                wordSpacing: 4,
-                                color: Colors.black,
-                                fontSize: 28,
-                                fontFamily: 'CantoraOne',
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ]),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-              Center(
-                child: Text(
-                  'Free slots :',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'Cantoraone',
-                      fontWeight: FontWeight.bold),
+            ),
+            Center(
+              child: Text(
+                'Free slots :',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'Cantoraone',
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 15),
-              GestureDetector(
-                onTap: () {
+            ),
+            SizedBox(height: 15),
+            GestureDetector(
+              onTap: () async {
+                // Call the API model method to reserve a slot
+                Map<String, dynamic> result = await ReservationApiModel.reserveSlot();
+                if (result['success']) {
+                  // Navigate to SuccessPage with the reservation code
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => SuccessPage()),
+                    MaterialPageRoute(builder: (context) => SuccessPage(reservationCode: result['code'])),
                   );
-                },
-                child: Padding(
-                  padding:
-                      const EdgeInsets.only(left: 70.0, right: 70, top: 20),
-                  child: Container(
-                    width: 70,
-                    height: 60,
-                    decoration: ShapeDecoration(
-                      color: Color.fromRGBO(88, 80, 141, 1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                } else {
+                  // Handle reservation failure if needed
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to reserve slot.')),
+                  );
+                }
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(left: 70.0, right: 70, top: 20),
+                child: Container(
+                  width: 70,
+                  height: 60,
+                  decoration: ShapeDecoration(
+                    color: Color.fromRGBO(88, 80, 141, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    child: Center(
-                      child: Text(
-                        'Reserve',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.white,
-                          fontFamily: 'Cantoraone',
-                          fontWeight: FontWeight.w500,
-                          height: 0,
-                        ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Reserve',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontFamily: 'Cantoraone',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
                       ),
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
