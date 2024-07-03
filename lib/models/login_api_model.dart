e4f4098cbc8b7755acb442f221fd51e98501d6df
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-07-03 23:01:37 +0300

    Update login_api_model.dart

--- lib/models/login_api_model.dart
+++ lib/models/login_api_model.dart
@@ -1,8 +1,11 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class LoginApiModel {
-  static Future<bool> login(String email, String password) async {
+  static final storage = FlutterSecureStorage();
+
+  static Future<String?> login(String email, String password) async {
     final url = Uri.parse('https://cse-parking.up.railway.app/api/login/');
     final response = await http.post(
       url,
@@ -16,9 +19,33 @@ class LoginApiModel {
     );
 
     if (response.statusCode == 200) {
-      return true; // login successful
+      // Login successful
+      Map<String, dynamic> responseData = jsonDecode(response.body);
+      String? token =
+          responseData['jwt']; // Assuming 'jwt' is the key for the token
+
+      if (token != null) {
+        // Store token securely
+        await storage.write(key: 'token', value: token);
+        // Print token to console (for demonstration purposes)
+        print('Token stored: $token');
+        return token;
+      } else {
+        print('Token not found in response');
+        return null;
+      }
     } else {
-      return false; // login failed
+      // Login failed
+      print('Failed to login. Status code: ${response.statusCode}');
+      return null;
     }
+  }
+
+  static Future<String?> getToken() async {
+    return await storage.read(key: 'token');
+  }
+
+  static Future<void> deleteToken() async {
+    await storage.delete(key: 'token');
   }
 }
