8fe58fce5099e9afedb512d862db1bcb12989512
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-07-05 02:42:40 +0300

    Add files via upload

---
+++ lib/models/duration_api_model.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:parking/models/login_api_model.dart';
+
+class DurationApiModel {
+  static final storage = FlutterSecureStorage();
+
+  static Future<Map<String, dynamic>> fetchDuration() async {
+    String? token = await LoginApiModel.getToken();
+    if (token == null) {
+      print('Token not found. Please login first.');
+      return {'success': false, 'durations': null};
+    }
+
+    final url = Uri.parse('https://cse-parking.up.railway.app/api/history/');
+    final response = await http.get(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Cookie': 'jwt=$token', // to set the JWT token in the Cookie header
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      final List<double> durations = responseData.map((e) => (e['duration'] as num).toDouble()).toList();
+      return {'success': true, 'durations': durations};
+    } else {
+      print('Failed to fetch durations. Status code: ${response.statusCode}');
+      return {'success': false, 'durations': null};
+    }
+  }
+}
